8f2a3072c46c76579d200f76179b56114e1848ed
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-25 10:32:44 +0900

    マテリアルUIのインポート

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
 class App extends Component {
   render() {
     return (
